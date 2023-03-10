eeb6fde853cce7877dede7a8a54786342cea8e0e
jorgehara <empresamodelo2018@gmail.com>
2023-03-09 23:18:15 -0300

    Finalizo paso props Total

--- react-y-types/src/App.tsx
+++ react-y-types/src/App.tsx
@@ -1,21 +1,65 @@
-import React, { useState } from 'react';
+import React, { MouseEventHandler, useState } from 'react';
 import './App.css';
-import { RamdomFox } from './Components/RamdomFox/RamdomFox';
+import { LazyImage } from './Components/LazyImage/LazyImage';
 
+// generate simple unique id
+const generateId = (): string => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+};
+
+// random number from 1 to 122
+const random = () => Math.floor(Math.random() * 122) + 1;
+
+type ImageItem = {
+  id: string;
+  url: string;
+};
 
 
 
 
 export const App = (): JSX.Element => {
 
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+    const id = generateId();
+    const url = `https://randomfox.ca/images/${random()}.jpg`;
+    setImages([...images, { id, url }]);
+  };
 
 
   return (
     <div className="App">
       <h1>Home!</h1>
-      <h1>Home!</h1><h1>Home!</h1>
-              <RamdomFox image='' />
-            </div>
-  
+      <button
+        onClick={addNewFox}
+        className="btn btn-primary text-white rounded mb-4 justify-content-center"
+      >Add new fox</button>
+      <div className="container text-center justify-content-center">
+        <div className="row gap-4 col-sm-12 col-md-12 col-lg-12 sizeImage rounded align-items-center justify-content-center">
+          <div
+            className="row gap-4 col-sm-12 col-md-10 col-lg-12 sizeImage rounded justify-content-center"
+          >
+            {images.map(({ id, url }) => (
+              <div 
+              className="row gap-4 col-sm-12 col-md-6 col-lg-6 rounded sizeImage justify-content-center" 
+              key={id}>
+                <LazyImage 
+                src={url} 
+                width="350"
+                height="auto"
+                alt="aqui aparece una imagen de un zorro"
+                onClick={() => console.log('hey')} 
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
-}
+};
---
+++ react-y-types/src/Components/LazyImage/LazyImage.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useRef, useEffect, useState } from "react";
+import type { ImgHTMLAttributes } from "react";
+import './LazyImage.css'
+
+type LazyImageProps = { src: string };
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
+type Props = LazyImageProps & ImageNative
+
+export const LazyImage = ({ src, ...imgProps }: Props): JSX.Element => {
+  // array simple
+  // const [images, setImages] = useState<Array<string>>([
+  //   `https://randomfox.ca/images/${random()}.jpg`, ])
+
+  // array con object
+  // const [images, setImages] = useState<Array<ImageItem>>([
+  //   { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+
+  // // array vacio a llenar
+  // const [images, setImages] = useState<Array<ImageItem>>([ ])
+
+  // useRef
+  const node = useRef<HTMLImageElement>(null);
+  const [currentSrc, setCurrentSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+
+  useEffect(() => {
+    // nuevo observador
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // onIntersection --> console.log
+        if (!entry.isIntersecting || !node.current) {
+          return;
+        }
+        setCurrentSrc(src)
+      });
+    });
+    // observador nodo
+    if (node.current) {
+      observer.observe(node.current)
+    }
+
+    // desconectar
+    return () => {
+      observer.disconnect();
+    }
+  }, [src])
+
+  return (
+    <>
+      <img
+        ref={node}
+        src={currentSrc}
+        className='rounded bg-light sizeImage'
+        {...imgProps}
+        />
+    </>
+  )
+}
